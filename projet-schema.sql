7ff733180bcb1823b05ac8fecc5b6d066aa6844a
acm <a-mango@users.noreply.github.com>
2023-12-11 23:54:55 +0100

    fix: typo

--- projet-schema.sql
+++ projet-schema.sql
@@ -38,7 +38,7 @@ CREATE TABLE customer
 --
 CREATE TABLE collaborator
 (
-    collaborator_id INT PRIMARY KEY REFERENCES person (person_id) ON UPDATE CASCADE ON CASCADE,
+    collaborator_id INT PRIMARY KEY REFERENCES person (person_id) ON UPDATE CASCADE ON DELETE CASCADE,
     email           VARCHAR(128)    NOT NULL UNIQUE CHECK (email ~ '^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$')
 );
 
