179f064105751b2478221b089c5d2846ec733cf7
mvitoriacoliveira <mvitoriacoliveira@gmail.com>
2023-12-12 22:51:51 +0100

    add views

--- projet-views.sql
+++ projet-views.sql
@@ -37,3 +37,46 @@ ON tr.technician_id = t.technician_id
 INNER JOIN object o
 ON o.object_id = r.object_id
 WHERE t.technician_id = 1;
+
+-- Nb of employees per role view
+
+CREATE OR REPLACE VIEW nb_employees_per_role AS
+SELECT 'Manager' AS role,
+       COUNT(*) AS nb_employees
+FROM manager
+UNION ALL
+SELECT 'Technician' AS role,
+       COUNT(*) AS nb_employees
+FROM technician
+UNION ALL
+SELECT 'Receptionist' AS role,
+       COUNT(*) AS nb_employees
+FROM receptionist;
+
+-- Manager view (plutot collaborator info)
+
+CREATE OR REPLACE VIEW collab_role_id_view AS
+SELECT 'Manager' AS role,
+       manager_id AS id
+FROM manager
+UNION ALL
+SELECT 'Technician' AS role,
+    technician_id AS id
+FROM technician
+UNION ALL
+SELECT 'Receptionist' AS role,
+       receptionist_id AS id
+FROM receptionist;
+
+CREATE OR REPLACE VIEW collab_info AS
+SELECT v.role, v.id, p.name, p.phone_no, c.email
+FROM collab_role_id_view v
+INNER JOIN person p ON v.id = p.person_id
+INNER JOIN collaborator c ON p.person_id = c.collaborator_id;
+
+-- Receptionist view
+
+CREATE OR REPLACE VIEW receptionist_view AS
+SELECT r.customer_id, r.reparation_id, o.name, o.fault_desc, r.quote_state, r.date_created, r.reparation_state, o.location
+FROM reparation r
+INNER JOIN object o ON r.object_id = o.object_id;
