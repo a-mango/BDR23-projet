8eebea8d29661f9b9a12bff5bb3ba07fcdaac051
acm <a-mango@users.noreply.github.com>
2024-01-08 15:36:59 +0100

    feat: add HeroIcons package

--- frontend/src/Navigation.js
+++ frontend/src/Navigation.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { WrenchIcon } from '@heroicons/react/24/solid'
 
 const Link = ({text, link}) => {
     return (
@@ -12,6 +13,7 @@ const Navigation = () => {
     return (
         <nav className="py-4 bg-amber-300 border-b border-amber-500 text-xl">
             <ul className="flex space-x-10 px-10">
+                <WrenchIcon className="h-8 w-8 text-gray-800 hover:text-black"/>
                 <Link text="Dashboard" link="/dashboard"/>
                 <Link text="Receptionist" link="/receptionist"/>
                 <Link text="Technician" link="/technician"/>
