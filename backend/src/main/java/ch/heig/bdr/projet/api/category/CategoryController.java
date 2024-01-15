4f6c7a2fcf775a81333b7eb2494e1e45a105a919
mvitoriacoliveira <mvitoriacoliveira@gmail.com>
2024-01-15 14:48:45 +0100

    add headers

--- backend/src/main/java/ch/heig/bdr/projet/api/category/CategoryController.java
+++ backend/src/main/java/ch/heig/bdr/projet/api/category/CategoryController.java
@@ -3,7 +3,9 @@ package ch.heig.bdr.projet.api.category;
 /**
  * CategoryController class
  *
- * @author Vitória Oliveira <maria.cosmodeoliveira@heig-vd.ch>
+ * @author Aubry Mangold <aubry.mangold@heig-vd.ch>
+ * @author Eva Ray <eva.ray@heig-vd.ch>
+ * @author Vitòria Cosmo De Oliviera <maria.cosmodeoliveira@heig-vd.ch>
  */
 public class CategoryController {
 }
--- backend/src/main/java/ch/heig/bdr/projet/api/customer/CustomerController.java
+++ backend/src/main/java/ch/heig/bdr/projet/api/customer/CustomerController.java
@@ -3,7 +3,9 @@ package ch.heig.bdr.projet.api.customer;
 /**
  * CustomerController class
  *
- * @author Vitória Oliveira <maria.cosmodeoliveira@heig-vd.ch>
+ * @author Aubry Mangold <aubry.mangold@heig-vd.ch>
+ * @author Eva Ray <eva.ray@heig-vd.ch>
+ * @author Vitòria Cosmo De Oliviera <maria.cosmodeoliveira@heig-vd.ch>
  */
 public class CustomerController {
 }
