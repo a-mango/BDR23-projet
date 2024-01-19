459414091e6bec7f2090f6a6f62330828eac5f8a
mvitoriacoliveira <mvitoriacoliveira@gmail.com>
2024-01-19 14:32:44 +0100

    feat: implementation

--- backend/src/main/java/ch/heig/bdr/projet/api/customer/CustomerService.java
+++ backend/src/main/java/ch/heig/bdr/projet/api/customer/CustomerService.java
@@ -2,11 +2,9 @@ package ch.heig.bdr.projet.api.customer;
 
 import ch.heig.bdr.projet.api.PostgresConnection;
 import ch.heig.bdr.projet.api.person.Person;
+import ch.heig.bdr.projet.api.utils.utils;
 
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 import java.util.ArrayList;
 
 /**
@@ -28,71 +26,86 @@ public class CustomerService {
     }
 
     Customer getCustomerById(String id){
-        try {
-            Statement statement = conn.createStatement();
-            String query = "SELECT * FROM Customer_info_view WHERE Customer_id = " + id;
-            ResultSet rs = statement.executeQuery(query);
-            if (rs.next()) {
-                Person p = new Person(rs.getInt("Customer_id"), rs.getString("phone_no"), rs.getString("name"), rs.getString("comment"));
-                return new Customer(p.personId, p.phoneNumber, p.name, p.comment, rs.getString("private_note"), rs.getBoolean("tos_accepted"));
-            } else {
-                return null;
+        String query = "SELECT * FROM customer_info_view WHERE customer_id = ?";
+        try (PreparedStatement pstmt = conn.prepareStatement(query)){
+            pstmt.setString(1, id);
+            try(ResultSet rs = pstmt.executeQuery()) {
+                if (rs.next()) {
+                    return newCustomerFromResultSet(rs);
+                } else {
+                    return null;
+                }
             }
         } catch (SQLException e){
-            System.out.println(e.getMessage());
+            utils.logError(e);
             return null;
         }
     }
 
     ArrayList<Customer> getCustomers(){
-        try {
-            Statement statement = conn.createStatement();
-            String query = "SELECT * FROM customer_info_view";
-            ResultSet rs = statement.executeQuery(query);
-            ArrayList<Customer> Customers = new ArrayList<>();
-            while (rs.next()) {
-                //System.out.println("inside while");
-                Person p = new Person(rs.getInt("Customer_id"), rs.getString("phone_no"), rs.getString("name"), rs.getString("comment"));
-                Customers.add(new Customer(p.personId, p.phoneNumber, p.name, p.comment, rs.getString("private_note"), rs.getBoolean("tos_accepted")));
+        String query = "SELECT * FROM customer_info_view";
+        try(PreparedStatement pstmt = conn.prepareStatement(query)) {
+            try(ResultSet rs = pstmt.executeQuery(query)) {
+                ArrayList<Customer> customers = new ArrayList<>();
+                while (rs.next()) {
+                    customers.add(newCustomerFromResultSet(rs));
+                }
+                return customers;
             }
-            return Customers;
-
         } catch (SQLException e){
-            System.out.println(e.getMessage());
+            utils.logError(e);
             return null;
         }
     }
 
     void updateCustomer(String id, Customer updatedCustomer){
-        try {
-            Statement statement = conn.createStatement();
-            String query = "UPDATE Customer SET phone_no = '" + updatedCustomer.phoneNumber + "', name = '" + updatedCustomer.name + "', comment = '" + updatedCustomer.comment + "', tos_accepted = '" + updatedCustomer.tosAccepted + "', private_note = '" + updatedCustomer.privateNote + "' WHERE customer_id = '" + id + "'  AND '" + updatedCustomer.tosAccepted + " 'IN (TRUE, FALSE)";
-            statement.executeQuery(query);
+        String query = "UPDATE customer SET phone_no =?, name =?, comment =?, tos_accepted =?, private_note =? WHERE customer_id =?  AND ? IN (TRUE, FALSE)";
+
+        try (PreparedStatement pstmt = conn.prepareStatement(query)) {
+            pstmt.setString(1, updatedCustomer.phoneNumber);
+            pstmt.setString(2, updatedCustomer.name);
+            pstmt.setString(3, updatedCustomer.comment);
+            pstmt.setBoolean(4, updatedCustomer.tosAccepted);
+            pstmt.setString(4, updatedCustomer.privateNote);
+            pstmt.setString(6, id);
+            pstmt.setString(7, updatedCustomer.tosAccepted ? "TRUE" : "FALSE");
+
+            pstmt.executeUpdate();
         } catch (SQLException e){
-            System.out.println(e.getMessage());
+            utils.logError(e);
         }
     }
 
     void deleteCustomer(String id){
-        try {
-            Statement statement = conn.createStatement();
-            String query = "DELETE FROM customer WHERE Customer_id = " + id + "; DELETE FROM person WHERE person_id = " + id;
-            statement.executeQuery(query);
-
+        String query = "DELETE FROM person WHERE person_id =?";
+        try (PreparedStatement pstmt = conn.prepareStatement(query)){
+            pstmt.setString(1, id);
+            pstmt.executeUpdate();
         } catch (SQLException e){
-            System.out.println(e.getMessage());
+            utils.logError(e);
         }
     }
 
     void createCustomer(Customer customer){
-        try {
-            Statement statement = conn.createStatement();
-            String query = "INSERT INTO person (phone_no, name, comment) VALUES ('" + customer.phoneNumber + "', '" + customer.name + "', '" + customer.comment + "'); INSERT INTO Customer (phone_no, name, comment) VALUES ('" + customer.phoneNumber + "', '" + customer.name + "', '" + customer.comment + "')";
-            statement.executeQuery(query);
+        String query = "CALL projet.InsertCustomer(?, ?, ?, ?, ?);";
 
+        try (PreparedStatement pstmt = conn.prepareStatement(query)){
+            pstmt.setString(1, customer.name);
+            pstmt.setString(2, customer.phoneNumber);
+            pstmt.setString(3, customer.comment);
+            pstmt.setBoolean(4, customer.tosAccepted);
+            pstmt.setString(5, customer.privateNote);
+
+            pstmt.executeUpdate();
         } catch (SQLException e){
-            System.out.println(e.getMessage());
+            utils.logError(e);
         }
+    }
+
+    protected Customer newCustomerFromResultSet(ResultSet rs) throws SQLException {
+        Person p = new Person(rs.getInt("customer_id"), rs.getString("phone_no"), rs.getString("name"), rs.getString("comment"));
+
+        return new Customer(p.personId, p.phoneNumber, p.name, p.comment, rs.getString("private_note"), rs.getBoolean("tos_accepted"));
     }
 }
 
