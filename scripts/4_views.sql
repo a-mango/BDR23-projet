b1ca9cb5ee4730c995ef58e37649c2bb29b7e20c
evarayHEIG <eva.ray@heig-vd.ch>
2024-01-20 18:05:10 +0100

    Add instread of insert triggers for views.

--- scripts/4_views.sql
+++ scripts/4_views.sql
@@ -45,6 +45,21 @@ CREATE TRIGGER update_collaborator_person_trigger
 INSTEAD OF UPDATE ON collab_info_view
 FOR EACH ROW EXECUTE PROCEDURE update_collaborator_person();
 
+-- Update person and collaborator when there is an insert on collab_info_view
+
+CREATE OR REPLACE FUNCTION update_collaborator_person_on_insert() RETURNS TRIGGER AS $$
+BEGIN
+
+    CALL InsertCollaborator(NEW.name, NEW.phone_no, NEW.comment, NEW.email, NEW.collaborator_id);
+
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER update_collaborator_person_trigger_on_insert
+INSTEAD OF INSERT ON collab_info_view
+FOR EACH ROW EXECUTE PROCEDURE update_collaborator_person_on_insert();
+
 -- View with the information that a receptionist can access
 CREATE OR REPLACE VIEW receptionist_view AS
 SELECT r.customer_id,
@@ -87,6 +102,21 @@ CREATE TRIGGER update_customer_person_trigger
 INSTEAD OF UPDATE ON customer_info_view
 FOR EACH ROW EXECUTE PROCEDURE update_customer_person();
 
+-- Update Person and Customer when there is an insert on customer_info_view
+
+CREATE OR REPLACE FUNCTION update_customer_person_on_insert() RETURNS TRIGGER AS $$
+BEGIN
+
+    CALL InsertCustomer(NEW.Name, NEW.phone_no, NEW.comment, NEW.tos_accepted, NEW.private_note);
+
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER update_customer_person_trigger_on_insert
+INSTEAD OF INSERT ON customer_info_view
+FOR EACH ROW EXECUTE PROCEDURE update_customer_person_on_insert();
+
 -- View with receptionist info
 CREATE OR REPLACE VIEW receptionist_info_view AS
 SELECT *
@@ -102,6 +132,21 @@ CREATE TRIGGER update_collaborator_person_receptionist_trigger
 INSTEAD OF UPDATE ON receptionist_info_view
 FOR EACH ROW EXECUTE PROCEDURE update_collaborator_person();
 
+-- Update person, collaborator and receptionist when there is an insert on receptionist_info_view
+
+CREATE OR REPLACE FUNCTION update_collaborator_person_receptionist_on_insert() RETURNS TRIGGER AS $$
+BEGIN
+
+    CALL InsertReceptionist(NEW.name, NEW.phone_no, NEW.comment, NEW.email);
+
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER update_collaborator_person_receptionist_trigger_on_insert
+INSTEAD OF INSERT ON receptionist_info_view
+FOR EACH ROW EXECUTE PROCEDURE update_collaborator_person_receptionist_on_insert();
+
 -- View with technician info
 CREATE OR REPLACE VIEW technician_info_view AS
 SELECT *
@@ -111,11 +156,26 @@ ON t.technician_id = c.collaborator_id
 INNER JOIN person p
 ON t.technician_id = p.person_id;
 
--- Update person, collaborator and technician when receptionist_info_view is updated
+-- Update person, collaborator and technician when technician_info_view is updated
 
 CREATE TRIGGER update_collaborator_person_receptionist_trigger
 INSTEAD OF UPDATE ON technician_info_view
 FOR EACH ROW EXECUTE PROCEDURE update_collaborator_person();
+
+-- Update person, collaborator and technician when there is an insert on technician_info_view
+
+CREATE OR REPLACE FUNCTION update_collaborator_person_technician_on_insert() RETURNS TRIGGER AS $$
+BEGIN
+
+    CALL InsertTechnician(NEW.name, NEW.phone_no, NEW.comment, NEW.email);
+
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER update_collaborator_person_technician_trigger_on_insert
+INSTEAD OF INSERT ON technician_info_view
+FOR EACH ROW EXECUTE PROCEDURE update_collaborator_person_technician_on_insert();
 
 -- View with manager info
 CREATE OR REPLACE VIEW manager_info_view AS
@@ -126,8 +186,23 @@ ON m.manager_id = c.collaborator_id
 INNER JOIN person p
 ON m.manager_id = p.person_id;
 
--- Update person, collaborator and technician when receptionist_info_view is updated
+-- Update person, collaborator and manager when manager_info_view is updated
 
 CREATE TRIGGER update_collaborator_person_manager_trigger
 INSTEAD OF UPDATE ON manager_info_view
 FOR EACH ROW EXECUTE PROCEDURE update_collaborator_person();
+
+-- Update person, collaborator and manager when there is an insert on manager_info_view
+
+CREATE OR REPLACE FUNCTION update_collaborator_person_manager_on_insert() RETURNS TRIGGER AS $$
+BEGIN
+
+    CALL InsertManager(NEW.name, NEW.phone_no, NEW.comment, NEW.email);
+
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER update_collaborator_person_manager_trigger_on_insert
+INSTEAD OF INSERT ON manager_info_view
+FOR EACH ROW EXECUTE PROCEDURE update_collaborator_person_manager_on_insert();
