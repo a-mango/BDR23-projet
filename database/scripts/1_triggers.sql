83289cc78b70dd20c6a587b087029973a94b806e
mvitoriacoliveira <mvitoriacoliveira@gmail.com>
2024-01-21 22:17:27 +0100

    rfr: move triggers from views to triggers

--- database/scripts/1_triggers.sql
+++ database/scripts/1_triggers.sql
@@ -154,3 +154,208 @@ BEFORE INSERT ON receptionist_language
 FOR EACH ROW
 EXECUTE FUNCTION insert_language_if_not_exists();
 
+
+-- Update person and collaborator when there is an update on collab_info_view
+CREATE OR REPLACE FUNCTION update_collaborator_person() RETURNS TRIGGER AS $$
+BEGIN
+    -- Update person table
+    UPDATE person
+    SET name = NEW.name, phone_no = NEW.phone_no, comment = NEW.comment
+    WHERE person_id = NEW.collaborator_id;
+
+    -- Update collaborator table
+    UPDATE collaborator
+    SET email = NEW.email
+    WHERE collaborator_id = NEW.collaborator_id;
+
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER update_collaborator_person_trigger
+    INSTEAD OF UPDATE ON collab_info_view
+    FOR EACH ROW EXECUTE PROCEDURE update_collaborator_person();
+
+-- Update person and collaborator when there is an insert on collab_info_view
+CREATE OR REPLACE FUNCTION update_collaborator_person_on_insert() RETURNS TRIGGER AS $$
+BEGIN
+
+    CALL InsertCollaborator(NEW.name, NEW.phone_no, NEW.comment, NEW.email, NEW.collaborator_id);
+
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER update_collaborator_person_trigger_on_insert
+    INSTEAD OF INSERT ON collab_info_view
+    FOR EACH ROW EXECUTE PROCEDURE update_collaborator_person_on_insert();
+
+-- Delete person and collaborator when there is an delete on collab_info_view
+CREATE OR REPLACE FUNCTION delete_collaborator_person_on_delete() RETURNS TRIGGER AS $$
+BEGIN
+
+    DELETE FROM person
+    WHERE person_id = OLD.collaborator_id;
+
+    RETURN OLD;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER delete_collaborator_person_trigger_on_delete
+    INSTEAD OF DELETE ON collab_info_view
+    FOR EACH ROW EXECUTE PROCEDURE delete_collaborator_person_on_delete();
+
+-- Update Person and Customer when there is an update on customer_info_view
+CREATE OR REPLACE FUNCTION update_customer_person() RETURNS TRIGGER AS $$
+BEGIN
+    -- Update person table
+    UPDATE person
+    SET name = NEW.name, phone_no = NEW.phone_no, comment = NEW.comment
+    WHERE person_id = NEW.customer_id;
+
+    -- Update customer table
+    UPDATE customer
+    SET tos_accepted = NEW.tos_accepted, private_note = NEW.private_note
+    WHERE customer_id = NEW.customer_id;
+
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER update_customer_person_trigger
+    INSTEAD OF UPDATE ON customer_info_view
+    FOR EACH ROW EXECUTE PROCEDURE update_customer_person();
+
+-- Update Person and Customer when there is an insert on customer_info_view
+CREATE OR REPLACE FUNCTION update_customer_person_on_insert() RETURNS TRIGGER AS $$
+BEGIN
+
+    CALL InsertCustomer(NEW.Name, NEW.phone_no, NEW.comment, NEW.tos_accepted, NEW.private_note);
+
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER update_customer_person_trigger_on_insert
+    INSTEAD OF INSERT ON customer_info_view
+    FOR EACH ROW EXECUTE PROCEDURE update_customer_person_on_insert();
+
+-- Delete person and customer when there is an delete on customer_info_view
+CREATE OR REPLACE FUNCTION delete_customer_person_on_delete() RETURNS TRIGGER AS $$
+BEGIN
+
+    DELETE FROM person
+    WHERE person_id = OLD.customer_id;
+
+    RETURN OLD;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER delete_customer_person_trigger_on_delete
+    INSTEAD OF DELETE ON customer_info_view
+    FOR EACH ROW EXECUTE PROCEDURE delete_customer_person_on_delete();
+
+
+-- Update person, collaborator and receptionist when receptionist_info_view is updated
+CREATE TRIGGER update_collaborator_person_receptionist_trigger
+    INSTEAD OF UPDATE ON receptionist_info_view
+    FOR EACH ROW EXECUTE PROCEDURE update_collaborator_person();
+
+-- Update person, collaborator and receptionist when there is an insert on receptionist_info_view
+CREATE OR REPLACE FUNCTION update_collaborator_person_receptionist_on_insert() RETURNS TRIGGER AS $$
+BEGIN
+
+    CALL createReceptionist(NEW.name, NEW.phone_no, NEW.comment, NEW.email, NEW.languages);
+
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER update_collaborator_person_receptionist_trigger_on_insert
+    INSTEAD OF INSERT ON receptionist_info_view
+    FOR EACH ROW EXECUTE PROCEDURE update_collaborator_person_receptionist_on_insert();
+
+-- Delete person and receptionist when there is an delete on receptionist_info_view
+CREATE OR REPLACE FUNCTION delete_receptionist_person_on_delete() RETURNS TRIGGER AS $$
+BEGIN
+
+    DELETE FROM person
+    WHERE person_id = OLD.receptionist_id;
+
+    RETURN OLD;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER delete_receptionist_person_trigger_on_delete
+    INSTEAD OF DELETE ON receptionist_info_view
+    FOR EACH ROW EXECUTE PROCEDURE delete_receptionist_person_on_delete();
+
+
+-- Update person, collaborator and technician when technician_info_view is updated
+CREATE TRIGGER update_collaborator_person_receptionist_trigger
+    INSTEAD OF UPDATE ON technician_info_view
+    FOR EACH ROW EXECUTE PROCEDURE update_collaborator_person();
+
+-- Update person, collaborator and technician when there is an insert on technician_info_view
+CREATE OR REPLACE FUNCTION update_collaborator_person_technician_on_insert() RETURNS TRIGGER AS $$
+BEGIN
+
+    CALL InsertTechnician(NEW.name, NEW.phone_no, NEW.comment, NEW.email);
+
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER update_collaborator_person_technician_trigger_on_insert
+    INSTEAD OF INSERT ON technician_info_view
+    FOR EACH ROW EXECUTE PROCEDURE update_collaborator_person_technician_on_insert();
+
+-- Delete person and technician when there is an delete on technician_info_view
+CREATE OR REPLACE FUNCTION delete_technician_person_on_delete() RETURNS TRIGGER AS $$
+BEGIN
+
+    DELETE FROM person
+    WHERE person_id = OLD.technician_id;
+
+    RETURN OLD;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER delete_technician_person_trigger_on_delete
+    INSTEAD OF DELETE ON technician_info_view
+    FOR EACH ROW EXECUTE PROCEDURE delete_technician_person_on_delete();
+
+
+-- Update person, collaborator and manager when manager_info_view is updated
+CREATE TRIGGER update_collaborator_person_manager_trigger
+    INSTEAD OF UPDATE ON manager_info_view
+    FOR EACH ROW EXECUTE PROCEDURE update_collaborator_person();
+
+-- Update person, collaborator and manager when there is an insert on manager_info_view
+CREATE OR REPLACE FUNCTION update_collaborator_person_manager_on_insert() RETURNS TRIGGER AS $$
+BEGIN
+
+    CALL InsertManager(NEW.name, NEW.phone_no, NEW.comment, NEW.email);
+
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER update_collaborator_person_manager_trigger_on_insert
+    INSTEAD OF INSERT ON manager_info_view
+    FOR EACH ROW EXECUTE PROCEDURE update_collaborator_person_manager_on_insert();
+
+-- Delete person and manager when there is an delete on manager_info_view
+CREATE OR REPLACE FUNCTION delete_manager_person_on_delete() RETURNS TRIGGER AS $$
+BEGIN
+
+    DELETE FROM person
+    WHERE person_id = OLD.manager_id;
+
+    RETURN OLD;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER delete_manager_person_trigger_on_delete
+    INSTEAD OF DELETE ON manager_info_view
+    FOR EACH ROW EXECUTE PROCEDURE delete_manager_person_on_delete();
